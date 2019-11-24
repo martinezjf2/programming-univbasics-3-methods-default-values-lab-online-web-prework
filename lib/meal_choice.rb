3a9c01455c4bf199bad0d80e9afe51468c0db7ac
Jeffrey Martinez <martinezjf2@gmail.com>
2019-11-24 06:05:17 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,10 +1,10 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
 
-def meal_choice(broccoli, macaroni, meat == meat)
+def meal_choice(veg1, veg2, meat = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{meat} with #{broccoli} and #{macaroni}."
+  
 end
-
 
 
